87b6a5838adb38c29a0f039df4e605c162d8ebd7
yoku2010 <yoku2010@gmail.com>
2014-01-02 17:26:53 +0530

    Add Workspace Functions

--- app/scripts/controllers/workspace.js
+++ app/scripts/controllers/workspace.js
@@ -2,5 +2,8 @@
 
 angular.module('yokuApp')
   .controller('WorkspaceCtrl', ['$scope', 'Workspace', function ($scope, Workspace) {
-    
+    $scope.workspace = Workspace.Workspace;
+    $scope.addWorkspace = function() {
+    	
+    }
   }]);
