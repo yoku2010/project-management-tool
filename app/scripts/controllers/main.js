202e6d1d24802fd3639300626c8fb53cb13c3033
yoku2010 <yoku2010@gmail.com>
2014-01-06 17:59:39 +0530

    Dashboard for workspace - DONE

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,16 @@
 angular.module('yokuApp')
   .controller('MainCtrl', ['$scope', 'Workspace', function ($scope, Workspace) {
     $scope.newWorkspaceType = [];
+    $scope.data = {};
+    $scope.showWorkspaceDetails = function(workspaceType) {
+      $scope.data.activeWorkspace = workspaceType;
+    }
+    $scope.hideWorkspaceDetails = function() {
+      $scope.data.activeWorkspace = null;
+    };
     for (var i = 0; i < Workspace.workspaceType.length; i++) {
+      // to show default workspace
+      //$scope.data.activeWorkspace = Workspace.workspaceType[0].name;
       $scope.newWorkspaceType[i] = {
         name: Workspace.workspaceType[i].name,
         icon: Workspace.workspaceType[i].icon,
